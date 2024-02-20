07a7b4b4f8a81401880b4e73115b58fb911f5050
Leopoldino19 <p.leopoldino03@gmail.com>
2024-02-20 15:21:12 -0300

    atualização

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 5000;
+let numeroSecreto = gerarNumeroAleatorio();
 
-// Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
-let chute;
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto
+}
 
-let tentativa = 1;
+exibirTextoNaTela('h1', 'Jogo do Número secreto')
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100')
 
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1 );
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto); 
+} 
 
-// enquanto o chute não for igual ao n.s
- while(chute != numeroSecreto){
-
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-
-     if (chute == numeroSecreto) {
-        break;
-    } else {
-         if(chute > numeroSecreto){
-             alert("O número secreto é menor que "+ chute);
-         }else{
-             alert("O numero secreto é maior que "+ chute);
-         }
-         // numeros de tentativas 
-         tentativa++
-     }
- }
-
- let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
- alert(`Acertou número secreto ${numeroSecreto} com ${tentativa} ${palavraTentativa}.`);
-
-// if(tentativa > 1){
-//     alert('Acertou número secreto '+ numeroSecreto+ ` com ${tentativa} tentativas`);
-// } else{
-//     alert('Acertou número secreto '+ numeroSecreto+ ` como uma ${tentativa} tentativa`);
-// }
+function gerarNumeroAleatorio(){
+    return parseInt(Math. random() * 10 + 1)
+}
